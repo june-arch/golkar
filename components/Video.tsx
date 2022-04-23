8f1419d9f0aa16dcd6b14be8c786c6211fc4c9ae
Juniko <official.juniko@gmail.com>
2022-04-23 19:17:44 +0700

    fix video wicth component

--- components/Video.tsx
+++ components/Video.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export default function Video ({payload} : Props){
     return(
-        <div className='flex flex-col w-[60vh] m-2'>
+        <div className='flex flex-col 2xl:w-[60vh] m-2'>
             <iframe className="h-[50vh] w-full" src={payload.path} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </div>
     )
